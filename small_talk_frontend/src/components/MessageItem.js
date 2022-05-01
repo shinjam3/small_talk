d1cb50089591437cf7587be3def3dcc9dd8e2c08
James Shin <51712817+shinjam3@users.noreply.github.com>
2022-05-01 12:50:49 -0400

    Update MessageItem.js

--- small_talk_frontend/src/components/MessageItem.js
+++ small_talk_frontend/src/components/MessageItem.js
@@ -1,34 +1,33 @@
-import React, {useContext, useState, useEffect} from 'react';
-import {GroupContext} from '../contexts/GroupContext';
-import {UserContext} from '../contexts/UserContext';
+import React, { useContext, useState, useEffect } from "react";
+import { GroupContext } from "../contexts/GroupContext";
+import { UserContext } from "../contexts/UserContext";
 
-function MessageItem({message}) {
-	const {getGroups} = useContext(GroupContext);
-	const {user} = useContext(UserContext);	
-	const [messageItemClass, setItemClass] = useState('');
-	
-	useEffect(() => {
-		if (message.body.includes('has left the group.')) getGroups(user._id);
-		
-		if (user.firstName===message.sentBy) {
-			setItemClass('text-box sent');
-		}
-		else {
-			setItemClass('text-box received');
-		};
-		// eslint-disable-next-line react-hooks/exhaustive-deps
-	}, []);
+function MessageItem({ message }) {
+  const { getGroups } = useContext(GroupContext);
+  const { user } = useContext(UserContext);
+  const [messageItemClass, setItemClass] = useState("");
 
-	return (
-		<div className={messageItemClass}>
-			<div className='text-box-top'>
-				<p>{message.sentBy}</p>
-				<p>{message.dateSent + ' ' + message.timeSent}</p>
-			</div>
+  useEffect(() => {
+    if (message.body.includes("has left the group.")) getGroups(user._id);
 
-			<p className='text-box-body'>{message.body}</p>
-		</div>
-	);
-};
+    if (user.firstName === message.sentBy) {
+      setItemClass("text-box sent");
+    } else {
+      setItemClass("text-box received");
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  return (
+    <div className={messageItemClass}>
+      <div className="text-box-top">
+        <p>{message.sentBy}</p>
+        <p>{message.dateSent + " " + message.timeSent}</p>
+      </div>
+
+      <p className="text-box-body">{message.body}</p>
+    </div>
+  );
+}
 
 export default MessageItem;
