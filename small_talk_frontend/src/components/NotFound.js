e7dafaa2188a9146ff663da00754c0ad59c4ed16
James Shin <51712817+shinjam3@users.noreply.github.com>
2022-05-01 12:30:48 -0400

    Update NotFound.js

--- small_talk_frontend/src/components/NotFound.js
+++ small_talk_frontend/src/components/NotFound.js
@@ -1,32 +1,34 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
-class NotFound extends Component {		
-	handleClick = () => {
-		this.props.history.goBack();
-	}
-	
-	render() {
-		return (
-			<div style={{fontFamily:'Courier New, Courier, monospace'}}>
-				<h1 style={h1Style}>Page Not Found.</h1>
-				<button style={btnStyle} onClick={this.handleClick}>Go Back</button>
-			</div>
-		);
-	}	
+class NotFound extends Component {
+  handleClick = () => {
+    this.props.history.goBack();
+  };
+
+  render() {
+    return (
+      <div style={{ fontFamily: "Courier New, Courier, monospace" }}>
+        <h1 style={h1Style}>Page Not Found.</h1>
+        <button style={btnStyle} onClick={this.handleClick}>
+          Go Back
+        </button>
+      </div>
+    );
+  }
 }
 
 const h1Style = {
-	textAlign: 'center',
-	margin: '75px',
-	marginBottom: '20px'
-}
+  textAlign: "center",
+  margin: "75px",
+  marginBottom: "20px",
+};
 
 const btnStyle = {
-	display: 'block',
-	margin: 'auto',
-	padding: '10px',
-	fontSize: '16px',
-	cursor: 'pointer'
-}
+  display: "block",
+  margin: "auto",
+  padding: "10px",
+  fontSize: "16px",
+  cursor: "pointer",
+};
 
 export default NotFound;
