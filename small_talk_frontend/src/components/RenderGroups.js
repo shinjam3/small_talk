bceff75b476dfd1ed36ee6dc262b4a12a9f63ebc
James Shin <51712817+shinjam3@users.noreply.github.com>
2022-05-01 12:08:23 -0400

    Update RenderGroups.js

--- small_talk_frontend/src/components/RenderGroups.js
+++ small_talk_frontend/src/components/RenderGroups.js
@@ -1,20 +1,10 @@
-import React, { useContext } from 'react';
-import {GroupContext} from '../contexts/GroupContext';
-import GroupItem from './GroupItem';
+import React, { useContext } from "react";
+import { GroupContext } from "../contexts/GroupContext";
+import GroupItem from "./GroupItem";
 
-function RenderGroups({history}) {
-	const {groups} = useContext(GroupContext);
-	return (
- 		groups.map(
-			(group) => (
-				<GroupItem
-					group={group}
-					key={group._id}
-					history={history}
-				/>
-			)
-		)
-	);
-};
+function RenderGroups({ history }) {
+  const { groups } = useContext(GroupContext);
+  return groups.map((group) => <GroupItem group={group} key={group._id} history={history} />);
+}
 
 export default RenderGroups;
