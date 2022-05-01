7f20fa7f5b566b7a60fd024ba1d9f6377934667d
James Shin <51712817+shinjam3@users.noreply.github.com>
2022-05-01 12:06:22 -0400

    Update RenderNotifications.js

--- small_talk_frontend/src/components/RenderNotifications.js
+++ small_talk_frontend/src/components/RenderNotifications.js
@@ -1,10 +1,10 @@
-import React from 'react';
-import NotificationItem from './NotificationItem';
+import React from "react";
+import NotificationItem from "./NotificationItem";
 
-function RenderNotifications({notifications}) {
-	return (
-		notifications.map(notification => <NotificationItem key={notification._id} notification={notification} />)
-	);
-};
+function RenderNotifications({ notifications }) {
+  return notifications.map((notification) => (
+    <NotificationItem key={notification._id} notification={notification} />
+  ));
+}
 
 export default RenderNotifications;
