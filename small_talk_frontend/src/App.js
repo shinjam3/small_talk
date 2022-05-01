0a68f4d8c808dfe070b1c481cad6dc3a1f6f8aa0
James Shin <51712817+shinjam3@users.noreply.github.com>
2022-05-01 13:19:53 -0400

    Update App.js

--- small_talk_frontend/src/App.js
+++ small_talk_frontend/src/App.js
@@ -1,71 +1,71 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Redirect, Switch } from 'react-router-dom';
+import React from "react";
+import { BrowserRouter as Router, Route, Redirect, Switch } from "react-router-dom";
 
-import './styles/App.css';
-import './styles/headerStyles.css';
-import './styles/loginStyles.css';
-import './styles/messagesStyles.css';
-import './styles/connectStyles.css';
-import './styles/profileNavStyles.css';
-import './styles/notificationStyles.css';
-import './styles/editStyles.css';
-import './styles/settingsStyles.css';
-import './styles/groupStyles.css';
-import './styles/groupSettingsStyles.css';
-import './styles/groupAddMemberStyles.css';
+import "./styles/App.css";
+import "./styles/headerStyles.css";
+import "./styles/loginStyles.css";
+import "./styles/messagesStyles.css";
+import "./styles/connectStyles.css";
+import "./styles/profileNavStyles.css";
+import "./styles/notificationStyles.css";
+import "./styles/editStyles.css";
+import "./styles/settingsStyles.css";
+import "./styles/groupStyles.css";
+import "./styles/groupSettingsStyles.css";
+import "./styles/groupAddMemberStyles.css";
 
-import {SocketProvider} from './contexts/SocketContext';
-import {UserProvider} from './contexts/UserContext';
-import {GroupProvider} from './contexts/GroupContext';
-import {MessagesProvider} from './contexts/MessagesContext';
+import { SocketProvider } from "./contexts/SocketContext";
+import { UserProvider } from "./contexts/UserContext";
+import { GroupProvider } from "./contexts/GroupContext";
+import { MessagesProvider } from "./contexts/MessagesContext";
 
-import NotFound from './components/NotFound';
-import Login from './components/Login';
-import Register from './components/Register';
-import Messages from './components/Messages';
-import Group from './components/Group';
-import Connect from './components/Connect';
-import Friends from './components/Friends';
-import Profile from './components/Profile';
-import AccountDeleted from './components/AccountDeleted';
-import PrivateRoute from './components/PrivateRoute';
+import NotFound from "./components/NotFound";
+import Login from "./components/Login";
+import Register from "./components/Register";
+import Messages from "./components/Messages";
+import Group from "./components/Group";
+import Connect from "./components/Connect";
+import Friends from "./components/Friends";
+import Profile from "./components/Profile";
+import AccountDeleted from "./components/AccountDeleted";
+import PrivateRoute from "./components/PrivateRoute";
 
-function App() {	
-	return (
-		<SocketProvider>
-			<UserProvider>
-				<GroupProvider>
-					<MessagesProvider>
-						<Router>
-							<Switch>
-								<PrivateRoute	path='/dashboard/messages' exact={true} component={Messages} />
-								
-								<PrivateRoute path='/dashboard/connect' exact={true} component={Connect} />
-								
-								<PrivateRoute path='/dashboard/friends' exact={true} component={Friends} />
-								
-								<PrivateRoute path='/dashboard/profile' exact={true} component={Profile} />
-								
-								<PrivateRoute path='/group/:id' exact={true} component={Group} />
-								
-								<Route path='/register' render={props => <Register {...props} />} />
-							
-								<Route path='/not-found' render={props => <NotFound {...props} />} />
-								
-								<Route path='/account-deleted' render={props => <AccountDeleted {...props} />} />
-								
-								<Route exact path='/' component={Login} />
-								
-								<Redirect from='/dashboard' to='/dashboard/messages' />
-								
-								<Redirect to='/not-found' />
-							</Switch>
-						</Router>
-					</MessagesProvider>
-				</GroupProvider>
-			</UserProvider>
-		</SocketProvider>
-	);
-};
+function App() {
+  return (
+    <SocketProvider>
+      <UserProvider>
+        <GroupProvider>
+          <MessagesProvider>
+            <Router>
+              <Switch>
+                <PrivateRoute path="/dashboard/messages" exact={true} component={Messages} />
+
+                <PrivateRoute path="/dashboard/connect" exact={true} component={Connect} />
+
+                <PrivateRoute path="/dashboard/friends" exact={true} component={Friends} />
+
+                <PrivateRoute path="/dashboard/profile" exact={true} component={Profile} />
+
+                <PrivateRoute path="/group/:id" exact={true} component={Group} />
+
+                <Route path="/register" render={(props) => <Register {...props} />} />
+
+                <Route path="/not-found" render={(props) => <NotFound {...props} />} />
+
+                <Route path="/account-deleted" render={(props) => <AccountDeleted {...props} />} />
+
+                <Route exact path="/" component={Login} />
+
+                <Redirect from="/dashboard" to="/dashboard/messages" />
+
+                <Redirect to="/not-found" />
+              </Switch>
+            </Router>
+          </MessagesProvider>
+        </GroupProvider>
+      </UserProvider>
+    </SocketProvider>
+  );
+}
 
 export default App;
