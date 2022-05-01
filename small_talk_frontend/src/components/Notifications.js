1249bae1c1c5ed7e5bcb5697594a193f678f58fd
James Shin <51712817+shinjam3@users.noreply.github.com>
2022-05-01 12:28:26 -0400

    Update Notifications.js

--- small_talk_frontend/src/components/Notifications.js
+++ small_talk_frontend/src/components/Notifications.js
@@ -1,43 +1,39 @@
-import React, {useContext, useState, useEffect} from 'react';
-import {UserContext} from '../contexts/UserContext';
-import RenderNotifications from './RenderNotifications';
+import React, { useContext, useState, useEffect } from "react";
+import { UserContext } from "../contexts/UserContext";
+import RenderNotifications from "./RenderNotifications";
 
 function Notifications() {
-	const {user, getCurrentUserInfo} = useContext(UserContext);
-	const [notificationContainerClass, setContainerClass] = useState('notification-container');
-	const [noNotificationsClass, setNoClass] = useState('no-notifications');
-	
-	
-	useEffect(() => {
-		getCurrentUserInfo(user._id);
-		// eslint-disable-next-line react-hooks/exhaustive-deps
-	}, []);
-	
-	
-	useEffect(() => {		
-		if (!user.notifications.length) {
-			setContainerClass('notification-container hide');
-			setNoClass('no-notifications show');
-		}
-		else {
-			setContainerClass('notification-container');
-			setNoClass('no-notifications');
-		};
-		// eslint-disable-next-line react-hooks/exhaustive-deps
-	}, [user]);
-	
+  const { user, getCurrentUserInfo } = useContext(UserContext);
+  const [notificationContainerClass, setContainerClass] = useState("notification-container");
+  const [noNotificationsClass, setNoClass] = useState("no-notifications");
 
-	return (
-		<div>
-			<h1 className='st-h1'>Notifications</h1>
-			
-			<div className={notificationContainerClass}>
-				<RenderNotifications notifications={user.notifications} />
-			</div>
-			
-			<p className={noNotificationsClass}>No Notifications</p>
-		</div>
-	);
-};
+  useEffect(() => {
+    getCurrentUserInfo(user._id);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  useEffect(() => {
+    if (!user.notifications.length) {
+      setContainerClass("notification-container hide");
+      setNoClass("no-notifications show");
+    } else {
+      setContainerClass("notification-container");
+      setNoClass("no-notifications");
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [user]);
+
+  return (
+    <div>
+      <h1 className="st-h1">Notifications</h1>
+
+      <div className={notificationContainerClass}>
+        <RenderNotifications notifications={user.notifications} />
+      </div>
+
+      <p className={noNotificationsClass}>No Notifications</p>
+    </div>
+  );
+}
 
 export default Notifications;
