204f4705d31c12832de04749d79c915fb63160cc
James Shin <51712817+shinjam3@users.noreply.github.com>
2022-05-01 14:27:08 -0400

    Update server.js

--- small_talk_backend/server.js
+++ small_talk_backend/server.js
@@ -1,41 +1,41 @@
-const config = require('config');
-const jwt = require('jsonwebtoken');
-const _ = require('lodash');
+const config = require("config");
+const jwt = require("jsonwebtoken");
+const _ = require("lodash");
 
-const server = require('./index');
-const io = require('socket.io')(server, {
-  cors: {origin: '*'}
+const server = require("./index");
+const io = require("socket.io")(server, {
+  cors: { origin: "*" },
 });
 
 // middleware to verify user authentication
-io.use(async (socket, next) => {	
-	try {
-		const token = socket.handshake.query.token;
-		const payload = jwt.verify(token, config.get('jwtPrivateKey'));
-		socket.userId = payload.id;
-		next();
-	}
-	catch (err) {}
+io.use(async (socket, next) => {
+  try {
+    const token = socket.handshake.query.token;
+    const payload = jwt.verify(token, config.get("jwtPrivateKey"));
+    socket.userId = payload.id;
+    next();
+  } catch (err) {}
 });
 
-
-io.on('connection', socket => {	
-	socket.on('logOut', () => {
-		socket.disconnect();
+io.on("connection", (socket) => {
+  socket.on("logOut", () => {
+    socket.disconnect();
   });
-	
-	socket.on('joinGroup', ({groupId}) => {
-		socket.join(groupId);
-	});
-	
-	socket.on('exitGroup', ({groupId}) => {
-		socket.leave(groupId);
-	});
-	
-	socket.on('sendMessage', ({message}) => {
-		const newMessage = new Message(_.pick(message, ['groupId', 'sentById', 'sentBy', 'body', 'dateSent', 'timeSent']));
-		newMessage.dateCreated = new Date();
-		io.in(message.groupId).emit('newMessage', newMessage);
-		newMessage.save();
-	});
+
+  socket.on("joinGroup", ({ groupId }) => {
+    socket.join(groupId);
+  });
+
+  socket.on("exitGroup", ({ groupId }) => {
+    socket.leave(groupId);
+  });
+
+  socket.on("sendMessage", ({ message }) => {
+    const newMessage = new Message(
+      _.pick(message, ["groupId", "sentById", "sentBy", "body", "dateSent", "timeSent"])
+    );
+    newMessage.dateCreated = new Date();
+    io.in(message.groupId).emit("newMessage", newMessage);
+    newMessage.save();
+  });
 });
