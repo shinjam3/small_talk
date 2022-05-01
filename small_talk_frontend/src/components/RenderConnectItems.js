014de246a5bd925cf368e8ea5c529404fd553fd7
James Shin <51712817+shinjam3@users.noreply.github.com>
2022-05-01 12:10:54 -0400

    Update RenderConnectItems.js

--- small_talk_frontend/src/components/RenderConnectItems.js
+++ small_talk_frontend/src/components/RenderConnectItems.js
@@ -1,15 +1,8 @@
-import React from 'react';
-import ConnectItem from './ConnectItem';
+import React from "react";
+import ConnectItem from "./ConnectItem";
 
-function RenderConnectItems({users}) {
-	return (
-		users.map(user => (
-			<ConnectItem 
-				key={user._id} 
-				otherUser={user}
-			/>
-		))
-	);
-};
+function RenderConnectItems({ users }) {
+  return users.map((user) => <ConnectItem key={user._id} otherUser={user} />);
+}
 
 export default RenderConnectItems;
