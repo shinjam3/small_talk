35b58388de3ecd1f8a81e897c8a1ba83c2702d02
James Shin <51712817+shinjam3@users.noreply.github.com>
2022-05-01 12:10:24 -0400

    Update RenderFriends.js

--- small_talk_frontend/src/components/RenderFriends.js
+++ small_talk_frontend/src/components/RenderFriends.js
@@ -1,10 +1,8 @@
-import React from 'react';
-import FriendItem from './FriendItem';
+import React from "react";
+import FriendItem from "./FriendItem";
 
-function RenderFriends({friends, history}) {
-	return (
-		friends.map(friend => <FriendItem key={friend._id} friend={friend} history={history} />)
-	)
+function RenderFriends({ friends, history }) {
+  return friends.map((friend) => <FriendItem key={friend._id} friend={friend} history={history} />);
 }
 
 export default RenderFriends;
